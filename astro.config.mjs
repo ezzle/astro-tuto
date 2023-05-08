b7baee620177b320bb16349a4e0db83d01e3a3ba
Tshitshi Kia Ntoni <info@ezzle.io>
2023-05-08 11:38:03 +0200

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), alpinejs()],
+  // custom server port
+  server: { port: 3003 }
+});
